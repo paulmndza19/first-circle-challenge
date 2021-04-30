05f87b822e77a3390bd55ac3461231f6ab0fa2ec
John Paul Mendoza <paul@aelogica.com>
2021-04-30 19:44:23 +0800

    Create relationships

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
+  has_many :line_items
+
   validates :customer_name, presence: true
   validates :customer_address, presence: true
   validates :total_price, presence: true, numericality: true
