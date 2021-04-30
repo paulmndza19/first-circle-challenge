7ca0b447d6d1cc4f9b5b17c71237751fae85a318
John Paul Mendoza <paul@aelogica.com>
2021-04-30 13:14:39 +0800

    Add scale and precision to line items quantity

--- db/migrate/20210430035957_create_line_items.rb
+++ db/migrate/20210430035957_create_line_items.rb
@@ -4,7 +4,7 @@ class CreateLineItems < ActiveRecord::Migration[5.1]
       t.references :order, foreign_key: true
       t.references :product, foreign_key: true
       t.integer :quantity
-      t.decimal :total
+      t.decimal :total, precision: 8, scale: 2
 
       t.timestamps
     end
