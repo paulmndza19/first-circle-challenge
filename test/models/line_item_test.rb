f7f435edba4f08caeecdcf689308139d5eef2544
John Paul Mendoza <paul@aelogica.com>
2021-04-30 16:16:36 +0800

    Use fixtures in line items test

--- test/models/line_item_test.rb
+++ test/models/line_item_test.rb
@@ -2,66 +2,64 @@ require 'test_helper'
 
 class LineItemTest < ActiveSupport::TestCase
 
-  product = Product.first
-  order = Order.first
 
   test "should not save line item without quantity" do
     line_item = LineItem.new
-    line_item.product_id = product.id
-    line_item.order_id = order.id
-    line_item.price = 99.99
-    line_item.total = 99.99
+    line_item.product = products(:mbp)
+    line_item.order = orders(:one)
+    line_item.price = products(:mbp).price
+    line_item.total = 1399.99
 
     refute line_item.save
   end
 
   test "should not save line item without total" do
     line_item = LineItem.new
-    line_item.product_id = product.id
-    line_item.order_id = order.id
-    line_item.price = 99.99
+    line_item.product = products(:mbp)
+    line_item.order = orders(:one)
+    line_item.price = products(:mbp).price
     line_item.quantity = 1
 
     refute line_item.save
   end
 
-  test "should not save line item without order_id" do
+  test "should not save line item without order" do
     line_item = LineItem.new
-    line_item.product_id = product.id
+    line_item.product = products(:mbp)
     line_item.quantity = 1
-    line_item.price = 99.99
-    line_item.total = 99.99
+    line_item.price = products(:mbp).price
+    line_item.total = 1399.99
 
     refute line_item.save
   end
 
-  test "should not save line item without product_id" do
+  test "should not save line item without product" do
     line_item = LineItem.new
-    line_item.order_id = order.id
+    line_item.order = orders(:one)
     line_item.quantity = 1
-    line_item.price = 99.99
-    line_item.total = 99.99
+    line_item.price = products(:mbp).price
+    line_item.total = 1399.99
 
     refute line_item.save
   end
 
   test "should not save line item without price" do
     line_item = LineItem.new
-    line_item.order_id = order.id
-    line_item.product_id = product.id
+    line_item.product = products(:mbp)
+    line_item.order = orders(:one)
     line_item.quantity = 1
-    line_item.total = 99.99
+    line_item.total = 1399.99
 
     refute line_item.save
   end
 
   test "total should be equal to price * quantity" do
     line_item = LineItem.new
-    line_item.product_id = product.id
-    line_item.order_id = order.id
+    line_item.product = products(:mbp)
+    line_item.order = orders(:one)
     line_item.quantity = 3
-    line_item.price = 50.00
-    line_item.total = 150.00
+    line_item.price = products(:mbp).price
+    line_item.total = 4199.97
 
     assert line_item.save
     assert_equal line_item.total, line_item.quantity * line_item.price
