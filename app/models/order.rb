75aba879b15989dde32b3ab6ce0ba87974fc6cb8
John Paul Mendoza <paul@aelogica.com>
2021-04-30 16:04:53 +0800

    Add numeric validation to order total price

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
   validates :customer_name, presence: true
   validates :customer_address, presence: true
-  validates :total_price, presence: true
+  validates :total_price, presence: true, numericality: true
 end
