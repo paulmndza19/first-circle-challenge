8f43bbf55bacd2c8964aa0ca3cea1d4b7cc76c17
John Paul Mendoza <paul@aelogica.com>
2021-04-30 18:41:12 +0800

    Create seeds for products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(sku: 'vga', name: 'VGA adapter', price: 30.00)
+Product.create(sku: 'atv', name: 'Apple TV', price: 109.50)
+Product.create(sku: 'mbp', name: 'MacBook Pro', price: 1399.99)
+Product.create(sku: 'ipd', name: 'Super iPad', price: 549.99)
