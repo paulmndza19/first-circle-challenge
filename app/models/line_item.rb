f328a45d88b82d30cd0b4b119607972907fb6029
John Paul Mendoza <paul@aelogica.com>
2021-04-30 16:04:17 +0800

    Add validation to line item model

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,8 @@
 class LineItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  validates :price, presence: true, numericality: true
+  validates :total, presence: true, numericality: true
+  validates :quantity, presence: true, numericality: true
 end
