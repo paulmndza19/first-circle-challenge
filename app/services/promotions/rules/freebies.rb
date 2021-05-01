34214b31fba28745da30edc176ed0c39276833de
John Paul Mendoza <paul@aelogica.com>
2021-05-01 13:55:20 +0800

    Fix error in freebies

--- app/services/promotions/rules/freebies.rb
+++ app/services/promotions/rules/freebies.rb
@@ -1,12 +1,16 @@
 module Promotions
   module Rules
     class Freebies
-      def initialize(promotion, line_item)
+      def self.call(promotion, line_item)
         @promotion = promotion
         @line_item = line_item
+
+        create_line_item
       end
 
-      def call
+      private
+
+      def self.create_line_item
         quantity = @line_item.quantity
 
         freebie_quantity_per_product = @promotion.freebie_quantity
@@ -16,8 +20,9 @@ module Promotions
         freebies_line_item = LineItem.new
         freebies_line_item.product_id = freebie_product_id
         freebies_line_item.quantity = quantity
+        freebies_line_item.price = 0
         freebies_line_item.total = 0
-        freebies_line_item.order = line_item.order
+        freebies_line_item.order = @line_item.order
 
         freebies_line_item.save
       end
