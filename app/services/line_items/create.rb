26ff33e6f0b4b5193463fb175c319da8b99f47e0
John Paul Mendoza <paul@aelogica.com>
2021-05-01 13:27:25 +0800

    Create service for creating line_items

---
+++ app/services/line_items/create.rb
@@ -0,0 +1,35 @@
+module LineItems
+  class Create
+    def self.call(order, line_item_attributes)
+      @order = order
+      @line_item_attributes = line_item_attributes
+
+      set_product
+      create_line_item
+      compute_for_promotion
+
+      @line_item
+    end
+
+    private
+
+    def self.set_product
+      id = @line_item_attributes[:product_id]
+      @product = Product.find(id)
+    end
+
+    def self.create_line_item
+      @line_item = @order.line_items.new
+      @line_item.quantity = @line_item_attributes[:quantity]
+      @line_item.price =  @product.price
+      @line_item.product = @product
+      @line_item.total = LineItems::ComputeTotal.call(@product, @line_item)
+
+      @line_item.save
+    end
+
+    def self.compute_for_promotion
+      LineItems::ComputePromotion.call(@product, @line_item)
+    end
+  end
+end
