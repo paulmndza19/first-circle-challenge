e13af4df963894f80f0d2ace7ab20838fa96a96c
John Paul Mendoza <paul@aelogica.com>
2021-05-01 13:50:14 +0800

    Fix bulk pricing

--- app/services/promotions/rules/bulk_pricing.rb
+++ app/services/promotions/rules/bulk_pricing.rb
@@ -1,28 +1,27 @@
 module Promotions
   module Rules
     class BulkPricing
-      def initialize(promotion, line_item)
+      def self.call(promotion, line_item)
         @promotion = promotion
         @line_item = line_item
-      end
 
-      def call
         return unless eligible?
 
         quantity = @line_item.quantity
 
-        minimum_bulk_quantity = @promotion.minimum_bulk_quantit
+        minimum_bulk_quantity = @promotion.minimum_bulk_quantity
         bulk_price = @promotion.bulk_price
 
         total = bulk_price * quantity
 
+        @line_item.price = bulk_price
         @line_item.total = total
         @line_item.save
       end
 
       private
 
-      def eligible?
+      def self.eligible?
         @line_item.quantity >= @promotion.minimum_bulk_quantity
       end
     end
