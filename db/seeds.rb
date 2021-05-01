3033650825fbfb1a5e6ed32a5257cb5e52619fbd
John Paul Mendoza <paul@aelogica.com>
2021-05-01 22:05:34 +0800

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(sku: 'vga', name: 'VGA adapter', price: 30.00)
-Product.create(sku: 'atv', name: 'Apple TV', price: 109.50)
-Product.create(sku: 'mbp', name: 'MacBook Pro', price: 1399.99)
-Product.create(sku: 'ipd', name: 'Super iPad', price: 549.99)
+vga = Product.create(sku: 'vga', name: 'VGA adapter', price: 30.00)
+atv = Product.create(sku: 'atv', name: 'Apple TV', price: 109.50)
+mbp = Product.create(sku: 'mbp', name: 'MacBook Pro', price: 1399.99)
+ipd = Product.create(sku: 'ipd', name: 'Super iPad', price: 549.99)
+
+
+ProductsPromotion.create(name: 'Buy 2 Get 1', buy: 2, get: 1, product: atv, promotion_rule: 'Promotions::Rules::BuyXGetY')
+ProductsPromotion.create(name: 'Bulk Pricing for Super iPad', minimum_bulk_quantity: 5, bulk_price: 499.99, product: ipd, promotion_rule: 'Promotions::Rules::BulkPricing')
+ProductsPromotion.create(name: 'MacBook Freebie', freebie_product_id: vga.id, freebie_quantity: 1, product: mbp, promotion_rule: 'Promotions::Rules::Freebies')
