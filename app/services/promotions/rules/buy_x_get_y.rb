e5a9d9b1d8bdeb6a6de7945d6694327fa959795a
John Paul Mendoza <paul@aelogica.com>
2021-05-01 20:57:51 +0800

    Create a new line item for buy x get y products

--- app/services/promotions/rules/buy_x_get_y.rb
+++ app/services/promotions/rules/buy_x_get_y.rb
@@ -7,23 +7,31 @@ module Promotions
 
         return unless eligible?
 
-        quantity = @line_item.quantity
-        buy = @promotion.buy
-        get = @promotion.get
-        product = @line_item.product
-        price = product.price
-
-        items_to_add = (quantity / buy) * get
-        new_quantity = quantity + items_to_add
-
-        @line_item.quantity = new_quantity
-        @line_item.save
+        create_line_item
       end
 
       private
 
       def self.eligible?
         @line_item.quantity >= @promotion.buy
+      end
+
+      def self.create_line_item
+        quantity = @line_item.quantity
+        buy = @promotion.buy
+        get = @promotion.get
+        product = @line_item.product
+
+        items_to_add = (quantity / buy) * get
+
+        additional_line_item = LineItem.new
+        additional_line_item.product = product
+        additional_line_item.quantity = items_to_add
+        additional_line_item.price = 0
+        additional_line_item.total = 0
+        additional_line_item.order = @line_item.order
+
+        additional_line_item.save
       end
     end
   end
