2b7eb0489dbcc9d90ca864a7f13fa8ead1c9167e
John Paul Mendoza <paul@aelogica.com>
2021-05-01 22:30:17 +0800

    Move orders create test

---
+++ test/system/orders/create_test.rb
@@ -0,0 +1,39 @@
+require "application_system_test_case"
+require "supports/orders/test_helper"
+
+module Orders
+  class CreateTest < ApplicationSystemTestCase
+    include Orders::TestHelper
+
+    setup do
+      visit 'orders/new'
+
+      @customer_name = 'Cade Cunningham'
+      @customer_address = 'Orlando, Florida'
+
+      assert_selector 'h3', text: 'New Order'
+      fill_in 'order[customer_name]', with: @customer_name
+      fill_in 'order[customer_address]', with: @customer_address
+    end
+
+    test 'create an order that satisfies all promotion rules' do
+      order_products_that_satisfies_all_promotions
+
+      submit
+
+      assert_selector '#customer-name', text: "Customer Name: #{@customer_name}"
+      assert_selector '#customer-address', text: "Customer Address: #{@customer_address}"
+
+      assert page.has_content? 'Apple TV (109.5)'
+      assert page.has_content? 'Apple TV (0.0)'
+      assert page.has_content? 'MacBook Pro (1399.99)'
+      assert page.has_content? 'VGA adapter (0.0)'
+      assert page.has_content? 'Super iPad (499.99)'
+      assert page.has_content? '$7,028.42'
+
+      order = Order.last
+
+      assert_equal 7028.42.to_d, order.total_price
+    end
+  end
+end
