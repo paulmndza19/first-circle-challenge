f37cf0b7377237297a9814d41d6002e5e00d3b78
John Paul Mendoza <paul@aelogica.com>
2021-05-03 22:00:02 +0800

    Change logic of freebies

--- app/services/promotions/rules/freebies.rb
+++ app/services/promotions/rules/freebies.rb
@@ -7,13 +7,44 @@ module Promotions
 
         return unless eligible?
 
-        create_line_item
+        order_has_freebie_product? ? adjust_freebie_product : create_line_item
       end
 
       private
 
       def self.eligible?
         @line_item.product == @promotion.product
+      end
+
+      def self.order_has_freebie_product?
+        freebie_product = @promotion.freebie_product
+        order = @line_item.order
+
+        order.line_items.where(product: freebie_product).exists?
+      end
+
+      def self.adjust_freebie_product
+        freebie_product = @promotion.freebie_product
+        order = @line_item.order
+
+        freebie_product_line_item = order.line_items.find_by(product: freebie_product)
+
+        return if freebie_product_line_item.nil?
+
+        freebie_quantity = freebie_product_line_item.quantity
+        product_quantity = @line_item.quantity
+
+        if product_quantity >= freebie_quantity
+          freebie_product_line_item.update!(quantity: product_quantity, total: 0, price: 0)
+        else
+          priced_quantity = freebie_quantity - product_quantity
+          total = priced_quantity * freebie_product_line_item.price
+
+          debugger
+
+          freebie_product_line_item.update!(total: total)
+        end
+
       end
 
       def self.create_line_item
--- test/services/promotions/rules/freebies_test.rb
+++ test/services/promotions/rules/freebies_test.rb
@@ -8,27 +8,49 @@ module Promotions
 
       test 'line item is added when product has freebies' do
         mbp = Product.find_by(sku: 'mbp')
-        create_line_item(mbp)
+        line_item = create_line_item(mbp)
 
         assert_difference("@order.line_items.count") do
-          Promotions::Rules::Freebies.call(@promo, @line_item)
+          Promotions::Rules::Freebies.call(@promo, line_item)
         end
 
-        expected_freebie_quantity = @line_item.quantity * @promo.freebie_quantity
+        expected_freebie_quantity = line_item.quantity * @promo.freebie_quantity
         freebie_product = @promo.freebie_product
 
-        new_line_item = @order.line_items.last
-        actual_quantity = new_line_item.quantity
+        freebie_line_item = @order.line_items.last
+        actual_quantity = freebie_line_item.quantity
 
         assert_equal expected_freebie_quantity, actual_quantity
       end
 
+      test 'freebie line item is modified when it is existing in the order' do
+        mbp = Product.find_by(sku: 'mbp')
+        vga = Product.find_by(sku: 'vga')
+
+        freebie_line_item = create_line_item(vga, 1)
+        product_line_item = create_line_item(mbp)
+
+        assert_difference("@order.line_items.count", 0) do
+          Promotions::Rules::Freebies.call(@promo, product_line_item)
+        end
+
+        expected_freebie_quantity = product_line_item.quantity * @promo.freebie_quantity
+        freebie_product = @promo.freebie_product
+
+        freebie_line_item.reload
+        actual_quantity = freebie_line_item.quantity
+
+        assert_equal expected_freebie_quantity, actual_quantity
+        assert_equal 0, freebie_line_item.total
+        assert_equal 0, freebie_line_item.price
+      end
+
       test 'line item is not added when product has no freebies' do
         vga = Product.find_by(sku: 'vga')
-        create_line_item(vga)
+        freebie_line_item = create_line_item(vga)
 
         line_items_count = @order.line_items.count
-        Promotions::Rules::Freebies.call(@promo, @line_item)
+        Promotions::Rules::Freebies.call(@promo, freebie_line_item)
 
         assert_equal line_items_count, @order.line_items.count
       end
@@ -43,16 +65,16 @@ module Promotions
         @order.save
       end
 
-      def create_line_item(product)
-        @product = @promo.product
+      def create_line_item(product, quantity=3)
+        line_item = @order.line_items.new
+        line_item.quantity = 3
+        line_item.price =  product.price
+        line_item.product = product
+        line_item.total = LineItems::ComputeTotal.call(product, line_item)
 
-        @line_item = @order.line_items.new
-        @line_item.quantity = 3
-        @line_item.price =  product.price
-        @line_item.product = product
-        @line_item.total = LineItems::ComputeTotal.call(@product, @line_item)
+        line_item.save!
 
-        @line_item.save!
+        line_item
       end
     end
   end
