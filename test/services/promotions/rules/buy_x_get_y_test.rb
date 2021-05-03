6bdb66a8fa11e27b699fc67dd2775dd860a45c66
John Paul Mendoza <paul@aelogica.com>
2021-05-03 09:50:46 +0800

    Change test case for buy x take y

--- test/services/promotions/rules/buy_x_get_y_test.rb
+++ test/services/promotions/rules/buy_x_get_y_test.rb
@@ -6,28 +6,45 @@ module Promotions
         create_order
       end
 
-      test 'line item is added when quantity is satisfied' do
+      test 'total price' do
         create_line_item(3)
 
-        assert_difference("@order.line_items.count") do
-          Promotions::Rules::BuyXGetY.call(@promo, @line_item)
-        end
+        old_total = @line_item.total
+        old_quantity = @line_item.quantity
 
-        expected_quantity = (@line_item.quantity / @promo.buy) * @promo.get
-
-        new_line_item = @order.line_items.last
-        actual_quantity = new_line_item.quantity
-
-        assert_equal expected_quantity, actual_quantity
-      end
-
-      test 'line item is added when quantity is not satisfied' do
-        create_line_item(1)
-
-        line_items_count = @order.line_items.count
         Promotions::Rules::BuyXGetY.call(@promo, @line_item)
 
-        assert_equal line_items_count, @order.line_items.count
+        @line_item.reload
+
+        product = @line_item.product
+        price = product.price
+
+        divisor = @promo.buy + @promo.get
+        priced_quantity = old_quantity - (old_quantity / divisor)
+        expected_total = price * priced_quantity
+        new_total = @line_item.total
+
+        refute_equal old_total, new_total
+        assert_equal expected_total, new_total
+      end
+
+      test 'additional quantity' do
+        create_line_item(5)
+
+        old_quantity = @line_item.quantity
+
+        Promotions::Rules::BuyXGetY.call(@promo, @line_item)
+
+        @line_item.reload
+
+        divisor = @promo.buy + @promo.get
+        priced_quantity = old_quantity - (old_quantity / divisor)
+        free_quantity = priced_quantity / @promo.buy
+        expected_quantity = priced_quantity + free_quantity
+        new_quantity = @line_item.quantity
+
+        assert_equal expected_quantity, new_quantity
+        refute_equal old_quantity, new_quantity
       end
 
       private
